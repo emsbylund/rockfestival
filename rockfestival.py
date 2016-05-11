e5bcce509253bd3c80797e0f617c4decee92fb45
sofialundkvist <sofialundkvist@outlook.com>
2016-05-11 16:06:54 +0200

    Routes

--- rockfestival.py
+++ rockfestival.py
@@ -1,3 +1,11 @@
 # *-* coding:utf-8 *-*
 
 import bottle
+
+@route('/schedule')
+def schedule():
+    return template('spelschema')
+
+@route('/festival_workers')
+def show_festival_workers():
+    return template('festivaljobbare')
