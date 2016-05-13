b691fadd45aadb2513ab85591184482aeef72138
sofialundkvist <sofialundkvist@outlook.com>
2016-05-13 12:11:40 +0200

    Ändrat port

--- rockfestival.py
+++ rockfestival.py
@@ -53,4 +53,4 @@ def server_static(filename):
     return static_file(filename, root='static')
 
 
-run(host='localhost', port=8080, debug=True, reloader=True)
+run(host='localhost', port=8000, debug=True, reloader=True)
